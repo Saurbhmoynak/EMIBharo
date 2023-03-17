a9d959ed5b494821cb3a22e1545e87b276ab48af
Saurbh Moynak <94280322+Saurbhmoynak@users.noreply.github.com>
2023-03-17 18:55:49 +0530

    files

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,42 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+}
+
+
+let header = document.querySelector("header");
+let nav = document.querySelector("nav");
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0)
+  nav.classList.toggle("text-color", window.scrollY > 0)
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('active');
+})
+
+
 VanillaTilt.init(document.querySelector(".card"), {
   max: 25,
   speed: 300
 });
 
-//It also supports NodeList
+
 VanillaTilt.init(document.querySelectorAll(".card"));
+
+
+// Testimonials section
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
