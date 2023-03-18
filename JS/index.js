0e820bdec1dad0e7e44a555a87236c9178edb28b
Saurbh Moynak <94280322+Saurbhmoynak@users.noreply.github.com>
2023-03-18 13:12:33 +0530

    update

--- JS/index.js
+++ JS/index.js
@@ -40,3 +40,43 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+
+
+//counting section
+
+const counters = document.querySelectorAll(".counter span");
+const container = document.querySelector(".counters-container");
+
+let activated = false;
+
+window.addEventListener("scroll", () => {
+  if (pageYOffset > container.offsetTop - container.offsetHeight - 400 && activated == false) {
+    counters.forEach(counter => {
+      counter.innerText = 0;
+      let count = 0;
+
+      function updateCount() {
+        const target = parseInt(counter.dataset.count);
+
+        if (count < target) {
+          count++;
+
+          counter.innerText = count;
+          setTimeout(updateCount, 5);
+        } else {
+          counter.innerText = target;
+        }
+      }
+      updateCount();
+
+      activated = true;
+    });
+
+  } else if (pageYOffset < container.offsetTop - container.offsetHeight - 500 || pageYOffset === 0 && activated === true) {
+    counters.forEach(counter => {
+      counter.innerText = 0;
+    });
+    activated = false;
+  }
+});
